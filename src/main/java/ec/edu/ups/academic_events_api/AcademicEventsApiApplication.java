fe7224475c7378e1d9060103ef42696433c3c67a
Milton082 <chuquimilton53@gmail.com>
2026-07-24 09:00:56 -0500

    Proyecto Iniciado

---
+++ src/main/java/ec/edu/ups/academic_events_api/AcademicEventsApiApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.academic_events_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicEventsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicEventsApiApplication.class, args);
+	}
+
+}
