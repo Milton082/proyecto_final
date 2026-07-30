00802b852b984aa10901392691ec4cdf4ba15493
Jefferson <jeffersonguerrero415@gmail.com>
2026-07-30 04:12:54 -0500

    fix: enable OpenAPI JWT config in production

--- src/main/java/ec/edu/ups/academic_events_api/security/config/OpenApiConfig.java
+++ src/main/java/ec/edu/ups/academic_events_api/security/config/OpenApiConfig.java
@@ -2,7 +2,6 @@ package ec.edu.ups.academic_events_api.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -11,30 +10,38 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
-@Profile("!prod")
 public class OpenApiConfig {
 
-        private static final String SECURITY_SCHEME = "bearerAuth";
+    private static final String SECURITY_SCHEME = "bearerAuth";
 
-        @Bean
-        public OpenAPI academicEventsOpenApi() {
-                SecurityScheme bearerScheme = new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT");
+    @Bean
+    public OpenAPI academicEventsOpenApi() {
 
-                return new OpenAPI()
-                                .info(new Info()
-                                                .title("Academic Events API")
-                                                .description(
-                                                                "API para la gestión de eventos académicos")
-                                                .version("1.0.0"))
-                                .components(new Components()
-                                                .addSecuritySchemes(
-                                                                SECURITY_SCHEME,
-                                                                bearerScheme))
-                                .addSecurityItem(
-                                                new SecurityRequirement()
-                                                                .addList(SECURITY_SCHEME));
-        }
+        SecurityScheme bearerScheme =
+                new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT");
+
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Academic Events API")
+                                .description(
+                                        "API para la gestión de eventos académicos"
+                                )
+                                .version("1.0.0")
+                )
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        SECURITY_SCHEME,
+                                        bearerScheme
+                                )
+                )
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(SECURITY_SCHEME)
+                );
+    }
 }
